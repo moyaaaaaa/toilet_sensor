e29cf2c1cbfce2f351fe4560e797967f82120153
moyaaaaaa <danmakubi@gmail.com>
2019-02-16 23:39:12 +0900

    fix: migrateファイル変更

--- db/migrate/20190216094756_create_sensors.rb
+++ db/migrate/20190216094756_create_sensors.rb
@@ -1,7 +1,7 @@
 class CreateSensors < ActiveRecord::Migration[5.2]
   def change
     create_table :sensors do |t|
-      t.string :serial_id, null: false
+      t.string :serial_id, null: false, limit: 50
       t.float :voltage
       t.boolean :opened, null:false, default: true
 
