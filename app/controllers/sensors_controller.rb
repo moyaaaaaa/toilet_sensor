77d8bb7f54edac1e57fdca394c483b03985b20e7
moyaaaaaa <danmakubi@gmail.com>
2019-02-17 00:36:45 +0900

    apiの戻りを変更

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -11,19 +11,17 @@ class SensorsController < ApplicationController
   # PATCH/PUT /sensors/1
   # PATCH/PUT /sensors/1.json
   def update
-    respond_to do |format|
-      if @sensor.update(sensor_params)
-        format.json { render :show, status: :ok, location: @sensor }
-      else
-        format.json { render json: @sensor.errors, status: :unprocessable_entity }
-      end
+    if @sensor.update(sensor_params)
+      render json: { status: :ok }
+    else
+      render json: { status: :unprocessable_entity }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_sensor
-      @sensor = Sensor.find_by(serial_id: params[:serial_id])
+      @sensor = Sensor.find_by!(serial_id: params[:serial_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
