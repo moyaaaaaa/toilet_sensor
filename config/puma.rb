52af18392e62633f57b071f484ae5a9baa51619a
moyaaaaaa <danmakubi@gmail.com>
2019-02-16 21:34:30 +0900

    chore:pumaのsocketファイルを作成するようにした

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "unix://tmp/sockets/puma.sock"
+
+worker_timeout 180
